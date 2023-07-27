e1cce4cd8d4ea1bc7e71ea230184ad3f95e5a4e6
Jane Palacios <saraipalaciosjane@gmail.com>
2023-07-26 20:32:49 -0400

    navegeichon y layout de agregar fragmento

--- app/src/main/java/com/example/roomej1modulo6/MainActivity.kt
+++ app/src/main/java/com/example/roomej1modulo6/MainActivity.kt
@@ -3,6 +3,20 @@ package com.example.roomej1modulo6
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+/*
+    [] Navigation
+    [] frgamento para agregar y
+*   [] fragmento otro para listar
+    [] interfaz para agregar nueva tarea
+*  [] Room
+*       []dao
+*       []base de datos
+*       []entity
+*
+    [] Recicler view para ver las tareas
+
+*
+* */
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)

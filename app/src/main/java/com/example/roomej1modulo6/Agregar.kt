266606b7fe4c74e431bb2e245831b021b2a037ac
Jane Palacios <saraipalaciosjane@gmail.com>
2023-07-26 22:28:57 -0400

    funciona bd se ve el primer fragmento

--- app/src/main/java/com/example/roomej1modulo6/Agregar.kt
+++ app/src/main/java/com/example/roomej1modulo6/Agregar.kt
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.roomej1modulo6.databinding.FragmentAgregarBinding
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -17,43 +20,38 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class Agregar : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    lateinit var binding: FragmentAgregarBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
 
+    }
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_agregar, container, false)
+
+        binding = FragmentAgregarBinding.inflate(layoutInflater,container,false)
+        initListener()
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment Agregar.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            Agregar().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    private fun initListener() {
+        binding.btnAgregar.setOnClickListener{
+            val texto = binding.edTarea.text.toString()
+
+            guardarTarea(texto)
+        }
     }
+
+    private fun guardarTarea(texto: String) {
+
+        val dao = TareaBaseDatos.getDatabase(requireContext()).getTaskDao()
+        val tarea = Tarea(texto, "fecha")
+        GlobalScope.launch { dao.insertarTarea(tarea) }
+    }
+
+
+
+
 }

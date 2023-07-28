ad231ca4c66f7a57b551e79d12b9ed76f34638e1
Jane Palacios <saraipalaciosjane@gmail.com>
2023-07-27 22:25:13 -0400

    view model ok sin usar

---
+++ app/src/main/java/com/example/roomej1modulo6/TareaVM.kt
@@ -0,0 +1,20 @@
+package com.example.roomej1modulo6
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+
+class TareaVM(aplicacion: Application): AndroidViewModel(aplicacion)  {
+    private val repositorio: Repositorio
+    init {
+        repositorio = Repositorio(TareaBaseDatos.getDatabase(aplicacion).getTaskDao())
+    }
+    fun obtenerTareas(): LiveData<List<Tarea>> {
+        return repositorio.getTareas()
+    }
+    fun insertarTarea(tarea: Tarea)= viewModelScope.launch{
+        repositorio.insertarTask(tarea)
+    }
+}
